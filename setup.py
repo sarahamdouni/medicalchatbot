5fbc9dd576f348b7192e23ffc2e50bb5f69539b7
sarahamdouni <amdounisarah8@gmail.com>
2025-05-03 23:18:36 +0100

    folder added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+setup(
+    name='medical chatbot',
+    version='0.0.0',
+    author='sarah',
+    author_email='amdounisarah8@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
